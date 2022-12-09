333e7c7a0ab5908d5b1a43797bcca24c312eb0c8
therockstarind <rohitkumarrohit96@gmail.com>
2022-12-09 20:58:17 +0530

    fix upload size limit

--- components/FileUploadConfirmation.tsx
+++ components/FileUploadConfirmation.tsx
@@ -18,23 +18,64 @@ export const FileUploadConfirmation = ({getFile, setFile, token}: {getFile: File
         };
             const loadingToaster = toast.loading('Loading...');
             getFile?.map(async (el: File) => {
+                
                 try {
                     let fileAsBinary = await fileReaderFunc(el);
                     let fileAsBuffer = Buffer.from(fileAsBinary, 'binary')
+                    if (el.size > 4000000) {
+                        let fileSize = el.size;
+                        const body = {
+                            item: {
+                                description: "a large file",
+                                name: el.name
+                            }
+                        }
+                        const { uploadUrl } = (await axios.post(`https://graph.microsoft.com/v1.0/me/drive/root:/Today Upload/${el.name}:/createUploadSession`, body, config)).data
 
-                    await axios.put(`https://graph.microsoft.com/v1.0/me/drive/root:/Today Upload/${el.name}:/content`, fileAsBuffer , config)
-                    
-                    toast.remove(loadingToaster);
-                    successToast()
-                    setFile(null)
+                        let offset = 0;
+
+                        while (offset < fileSize) {
+                            let chunkSize = Math.min(fileSize - offset, 4 * 1024 * 1024);
+                            await uploadChunk(uploadUrl, el, offset, chunkSize, fileSize);
+                            toast.loading(`${Math.floor((offset/fileSize) * 100)}% completed`, {
+                                id: loadingToaster
+                            });
+                            offset += chunkSize;
+                        }
+                        toast.remove(loadingToaster);
+                        successToast()
+                        setFile(null)
+                    } else {
+                        await axios.put(`https://graph.microsoft.com/v1.0/me/drive/root:/Today Upload/${el.name}:/content`, fileAsBuffer , config)
+                        
+                        toast.remove(loadingToaster);
+                        successToast()
+                        setFile(null)
+                    }
                 }
                 catch (e: any) {
+                    console.log(e)
                     toast.remove(loadingToaster);
-                    errorToast(e.response.data.message)
+                    errorToast(e.response?.data?.message)
                 }
             })
             
         
+    }
+
+    async function uploadChunk(uploadUrl, fileData, offset, chunkSize, fileSize) {
+        let resultOfChunk = await fileReadForLargeFiles(fileData, offset, chunkSize)
+        var response = await axios.put(uploadUrl, 
+          resultOfChunk,  
+        {
+          headers: {
+            "Content-Type": "application/octet-stream",
+            "Content-Length": chunkSize,
+            "Content-Range": "bytes " + offset + "-" + (offset + chunkSize - 1) + "/" + fileSize
+          },
+        });
+      
+        return response;
     }
 
     const fileReaderFunc = (file: File): Promise<any> => {
@@ -49,32 +90,44 @@ export const FileUploadConfirmation = ({getFile, setFile, token}: {getFile: File
         })
     }
 
+    const fileReadForLargeFiles = (file: File, offset, chunk) => {
+        return new Promise((resolve, reject) => {
+            const filereader = new FileReader();
+
+            filereader.onload = () => {
+                resolve(filereader.result)
+            }
+            var slice = file.slice(offset, offset + chunk);
+            filereader.readAsArrayBuffer(slice);
+        })
+    }
+
 
     return (
         <>
-                            <div className='flex flex-col items-center px-3 gap-3 text-black'>
+            <div className='flex flex-col items-center px-3 gap-3 text-black'>
                                 <span className="text-black dark:text-white">Files Details</span>
-                                <>
-                                    {getFile.map((el: File, i: number, arr) => {
-                                        return (
-                                            <div className='flex flex-col items-center border dark:border-gray-700 rounded-xl px-3 py-2 bg-white' key={i}>
-                                                <div className=''>
-                                                File Name: {el.name}
-                                                </div>
-                                                <div className=''>
-                                                File Type: {el.type}
-                                                </div>
-                                            </div>
-                                        )
-                                       
-                                    })}
-                                </>
-                               
-                                <div className='flex justify-around gap-5'>
-                                    <button onClick={cancelUploadHandler} className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Cancel</button>
-                                    <button onClick={sendHandler} className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Upload</button>
+                <>
+                    {getFile.map((el: File, i: number, arr) => {
+                        return (
+                            <div className='flex flex-col items-center border dark:border-gray-700 rounded-xl px-3 py-2  bg-white' key={i}>
+                                <div className=''>
+                                File Name: {el.name}
+                                </div>
+                                <div className=''>
+                                File Type: {el.type}
                                 </div>
                             </div>
+                        )
+                        
+                    })}
+                </>
+                
+                <div className='flex justify-around gap-5'>
+                    <button onClick={cancelUploadHandler} className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Cancel</button>
+                    <button onClick={sendHandler} className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Upload</button>
+                </div>
+            </div>
         </>
     )
 }
